1cb31450115fca4ed7909ab41a61a7543dbb6cc1
Rija Lalaina <rijalaraz@gmail.com>
2026-06-13 16:15:11 +0300

    List

--- app/Filament/Resources/OrderItems/OrderItemResource.php
+++ app/Filament/Resources/OrderItems/OrderItemResource.php
@@ -18,7 +18,7 @@ class OrderItemResource extends Resource
 {
     protected static ?string $model = OrderItem::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::ClipboardDocumentList;
 
     protected static ?string $recordTitleAttribute = 'Détails des commandes';
 
