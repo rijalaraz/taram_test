9682e990ae8a833cc68f256009a8c3aceeda371c
Rija Lalaina <rijalaraz@gmail.com>
2026-06-18 15:13:45 +0300

    Correct faker images

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * @extends Factory<Product>
@@ -17,12 +18,19 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
+        $filename = 'products/' . fake()->uuid() . '.png';
+
+        Storage::disk('public')->put(
+            $filename,
+            file_get_contents('https://picsum.photos/300')
+        );
+
         return [
             'sku' => $this->faker->unique()->bothify('SKU-####'),
             'name' => $this->faker->words(3, true),
             'description' => $this->faker->paragraph(),
             'price' => $this->faker->randomFloat(2, 1, 100),
-            'image_url' => $this->faker->imageUrl(),
+            'image_url' => $filename,
             'is_active' => $this->faker->boolean(80), // 80% chance of being active
             'category_id' => \App\Models\Category::factory(), // Assuming you have a Category factory
         ];
