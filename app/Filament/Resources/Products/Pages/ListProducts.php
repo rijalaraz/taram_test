45fbc1b7b64b689772e9def5917336b1895468f7
Rija Lalaina <rijalaraz@gmail.com>
2026-06-19 01:14:26 +0300

    Move the import button

--- app/Filament/Resources/Products/Pages/ListProducts.php
+++ app/Filament/Resources/Products/Pages/ListProducts.php
@@ -3,7 +3,11 @@
 namespace App\Filament\Resources\Products\Pages;
 
 use App\Filament\Resources\Products\ProductResource;
+use App\Services\ExcelImageExtractor;
+use Filament\Actions\Action;
 use Filament\Actions\CreateAction;
+use Filament\Forms\Components\FileUpload;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
 
 class ListProducts extends ListRecords
@@ -14,6 +18,26 @@ class ListProducts extends ListRecords
     {
         return [
             CreateAction::make(),
+            Action::make('prepareImport')
+                ->label('Préparer import XLSX')
+                ->form([
+                    FileUpload::make('file')
+                        ->disk('local')
+                        ->directory('imports')
+                        ->required(),
+                ])
+                ->action(function (array $data) {
+
+                    $file = storage_path('app/private/' . $data['file']);
+
+                    app(ExcelImageExtractor::class)
+                        ->extract($file);
+
+                    Notification::make()
+                        ->title('Imported successfully')
+                        ->success()
+                        ->send();
+                }),
         ];
     }
 }
