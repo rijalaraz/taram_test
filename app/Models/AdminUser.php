c24a42946af46dc22b8c4a93fed4c11fd6bc3dd4
Rija Lalaina <rijalaraz@gmail.com>
2026-06-21 23:30:18 +0300

    little change

--- app/Models/AdminUser.php
+++ app/Models/AdminUser.php
@@ -6,13 +6,5 @@ use Chiiya\FilamentAccessControl\Models\FilamentUser;
 
 class AdminUser extends FilamentUser
 {
-    /**
-     * Return a name.
-     *
-     * Needed for compatibility with filament-logger.
-     */
-    public function getNameAttribute(): string
-    {
-        return 'filament';
-    }
+    
 }
