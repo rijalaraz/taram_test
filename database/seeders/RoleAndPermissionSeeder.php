ab6ffae29652a66cf22d01ebb196c1f32ab979f9
Rija Lalaina <rijalaraz@gmail.com>
2026-06-22 19:20:18 +0300

    Little change

--- database/seeders/RoleAndPermissionSeeder.php
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -12,7 +12,7 @@ use Spatie\Permission\Models\Role;
 class RoleAndPermissionSeeder extends Seeder
 {
     /**
-     * @var array<int, RoleName>
+     * @var array<int, string>
      */
     protected static array $roles = [RoleName::SUPER_ADMIN];
 
